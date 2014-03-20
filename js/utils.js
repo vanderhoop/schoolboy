d51645fe8f47d7d77fabc8409a5abf204dde5296
Travis Vander Hoop <vanderhoop@me.com>
2014-03-19 23:12:32 -0400

    added weekday validation function

--- js/utils.js
+++ js/utils.js
@@ -11,9 +11,17 @@ function validTimeForIntercept(){
     return true;
   } // end if
   // if between 9:00 a.m. and 5:30 p.m.
-  if (hour >= 8 && hour < 23){
+  if (hour >= 8 && hour <= 23){ // (changed to 23 for late-night dev purposes)
     return true;
   } else {
     return false;
   } // end if
 } // validTimeForIntercept
+
+// I want to write a function that returns whether it's a weekday
+
+function weekday(){
+  var dateObj = new Date(),
+      day = dateObj.getDay();
+      (if day > 0 && day < 6){ return true; }
+}
